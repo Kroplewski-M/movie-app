300e1f279a914dcd03d006e4ef040a5734a4ea9b
Mateusz <kroplewskimateusz@gmail.com>
2022-08-24 18:15:15 +0100

    fixed mobile positioning

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -1,16 +1,18 @@
 <template>
-    <section class=" w-[100%] h-[110vh] md:h-[110vh] bg-[#121212] text-green-400">
+    <section class=" w-[100%] h-[270vh] md:h-[110vh] bg-[#121212] text-green-400">
         <router-link to="/"><img src="@/resources/back.png" alt="" class="w-[50px] h-[50px] ml-5 mt-[20px] absolute"></router-link>
-        <div class="md:w-[1500px] md:h-[1000px] mx-auto pt-[80px]">
+        <div class="md:w-[1500px] md:h-[1000px] mx-auto pt-[80px] pl-5">
             <div class="w-[100%] md:h-[750px] h-[auto] bg-[#232323] rounded-md">
                 <img :src="this.movieImage" alt="" class="md:w-[500px] w-[350px] md:h-[700px] rounded-md md:float-left mr-5 ml-5 mt-5">
                 <p class="font-bold text-[50px] ml-10 pt-5">{{this.movie}}</p>
                 <img src="@/resources/calender.png" alt="" class="w-[25px] h-[25px] float-left mt-[49px] mr-[5px]">
-                <p class="mt-[50px] float-left text-green-500 mr-5">Release date: <span class="font-bold">{{this.movieRelease}}</span></p>
-                <img src="@/resources/vote.png" alt="" class="w-[25px] h-[25px] float-left mt-[49px] mr-[5px]">
-                <p class="mt-[50px] float-left text-green-500">Vote Average: <span class="ml-[10px] font-bold">{{this.movieAverage}} </span></p>
-                <p class="font-bold mt-[90px] text-green-500 text-[30px]">Description:</p>
-                <p class="md:float-left w-[300px] md:w-[700px] mt-[20px] text-[17px] text-green-500 italic pb-10 md:pb-0">{{this.movieDesc}}</p>
+                <p class="mt-[50px] float-left text-green-500">Release date: <span class="font-bold mr-5 md:mr-5">{{this.movieRelease}}</span></p>
+                <div class="float-left md:float-none">
+                    <img src="@/resources/vote.png" alt="" class="w-[25px] h-[25px] float-left mt-[49px] mr-[5px]">
+                    <p class="mt-[50px] float-left  text-green-500">Vote Average: <span class="ml-[10px] font-bold">{{this.movieAverage}} </span></p>
+                </div>
+                <p class="font-bold float-left mt-[90px] text-green-500 text-[30px] w-[100%] md:w-[800px]">Description:</p>
+                <p class="float-left w-[300px] md:w-[700px] mt-[20px] text-[17px] text-green-500 italic pb-10 md:pb-0 block">{{this.movieDesc}}</p>
             </div>
         </div>
     </section>
